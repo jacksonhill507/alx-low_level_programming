f7db1b3544bf60bc90b860902bb76a32b00ff0e9
Jackson Okpeku <jacksonchurchill507@gmail.com>
2022-03-10 22:41:28 +0000

    My commit message on 4 put task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #!/bin/bash
 #include <stdio.h>
-  int main (void){
-     puts("\"Programming is like building a multilingual puzzle");
-         return 0;
+/**
+ *main - entry point
+ *
+ *Return: 0 after printing the function
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
