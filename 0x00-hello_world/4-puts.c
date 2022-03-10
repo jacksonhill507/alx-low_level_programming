8fe0dc56c9a010d6668833d045f26ba21c2aae91
Jackson Okpeku <jacksonchurchill507@gmail.com>
2022-03-10 22:13:26 +0000

    My commit message on hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,6 @@
 #!/bin/bash
 #include <stdio.h>
-/**
- *main - entry point
-*
-*Return: o after printing the function
- */
-int main(void)
-{
-puts("\"programming is like building a multilingual puzzle");
-return (0);
+  int main (void){
+     puts("\"Programming is like building a multilingual puzzle");
+         return 0;
 }
