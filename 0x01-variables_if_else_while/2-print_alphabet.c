bf490383b4408d88b76153b6271298207ca43c3a
Jackson Okpeku <jacksonchurchill507@gmail.com>
2022-03-12 23:58:55 +0000

    My alphabeth commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+
+{
+char alphabet:
+
+for (alphabet = 'a'; alphabeth <= 'z'; alphabet++)
+{
+putchar(alphabet);
+}
+putchar('\n');
+
+return (0);
+
+}
