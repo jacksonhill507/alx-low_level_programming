7f28c4e0f13479c550438c3f5175be5c9aa48e12
jacksonhill507 <jacksonchurchill507@gmail.com>
2022-03-24 21:27:04 +0100

    my commit on task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * leet - function that encodes a string into 1337
+ * @str: character to be checked
+ * Return: If seperator return 1. Otherwise return 0;
+ */
+char *leet(char *str)
+{
+
+int count = 0, i = 0;
+char *letters = "aeotlAEOTL";
+char *nums = "4307143071";
+
+
+while (str[count] != '\0')
+{
+i = 0;
+while (letters[i] != '\0')
+{
+if (str[count] == letters[i])
+str[count] = nums[i];
+i++;
+}
+count++;
+}
+
+return (str);
+}
