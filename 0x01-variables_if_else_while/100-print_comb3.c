9f1731b01f980eec5c1bf9262e1a6a0d54d06113
jacksonhill507 <jacksonchurchill507@gmail.com>
2022-03-25 16:48:36 +0100

    my commit message on task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,44 +2,40 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success/completed)
+ * Return: Always 0 (Success)
  */
 int main(void)
 
 {
-int number_left;
+int number_left; /*Decarling statement*/
 int number_right;
 
+/*for 0-9 with ASCII*/
 
-for (number_left = 48; number_right <= 78; number_left++)
+for (number_left = 48; number_left <= 57; number_left++)
 {
-
-
-
-for (number_right = number_left + 1 ; number_right <= 78; number_right++)
+for (number_right = number_left + 1 ; number_right <= 57; number_right++)
 {
 
 putchar(number_left);
 putchar (number_right);
 
-
-if ((number_left == 56) && (number_right == 78))
+/*break program if the condition is true (if is 99=+)*/
+if ((number_left == 56) && (number_right == 57))
 {
-break;
+break;/*terminate the program*/
 }
 
-putchar(',');
-putchar (' ');
+putchar(','); /*print (,)*/
+putchar (' '); /*prirint a space*/
 
 }
 
 }
 
-putchar('\n');
-
+putchar('\n'); /*new line*/
 
 return (0);
-
 
 }
 
