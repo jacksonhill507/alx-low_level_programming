cd4bcd3d425c67e5dd03e8bc122a120c3991129a
jacksonhill507 <jacksonchurchill507@gmail.com>
2022-04-15 14:34:19 +0100

    my commit message on variadic functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,29 +1,26 @@
-#ifndef _VARIADIC_FUNCTIONS_H
-#define _VARIADIC_FUNCTIONS_H
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
 #include <stdarg.h>
-#include <stdio.h>
 
 /**
-* struct fmt - Struct format
-* @f: type format
-* @p: print function
-*/
-typedef struct fmt
+ * struct printer - A new struct type defining a printer.
+ * @symbol: A symbol representing a data type.
+ * @print: A function pointer to a function that prints
+ *         a data type corresponding to symbol.
+ */
+typedef struct printer
 {
-	char *f;
-	void (*p)(va_list s);
-} my_fmt;
+	char *symbol;
+	void (*print)(va_list arg);
 
+} printer_t;
 
-/*prototypes*/
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-int _strlen_recursion(char *s);
-void print_c(va_list s);
-void print_i(va_list s);
-void print_f(va_list s);
-void print_s(va_list s);
 
-#endi
+#endif
+
+
